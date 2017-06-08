35092c5297860d52daba7b8dbc8ed4c301913c37
Dylan Mäenpää <jdylanm96@gmail.com>
2017-06-08 13:52:49 +0200

    WelcomeScreen done! close #3

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Rewire your brain to become more positive!', color: '#8BC8DB' },
+  { text: 'Write down your gratitudes everyday!', color: '#7CD175' }
+];
+
 
 class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('main');
+  }
+
   render() {
     return (
-      <View>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
+      <View
+        style={{ flex: 1 }}
+        backgroundColor='#8BC8DB'
+      >
+        <Slides
+          data={SLIDE_DATA}
+          onComplete={this.onSlidesComplete}
+        />
       </View>
     );
   }
